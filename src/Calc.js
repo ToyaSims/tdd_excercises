e97217cfd563b0ab4b19dd7be3352ece43877e47
Latifa_Simbini <simbinelatifa@gmail.com>
2019-05-27 16:31:24 +0200

    Update Calc.js

--- src/Calc.js
+++ src/Calc.js
@@ -1,3 +1,16 @@
+//function adds 2 numbers and return 0
+function add(num1, num2) {
+    return num1 + num2;
+
+}
+
+//function adds 2 negetive numbers
+function add(num1, num2) {
+    return num1 + num2;
+
+}
+
+
 //function adds 2 numbers
 function add(num1, num2) {
     return num1 + num2;
@@ -21,7 +34,7 @@ function multiply(num1, num2) {
 
 }
 
-//function multiplies 2 numbers
+//function multipliesall numbers entered
 function multiplyAll() {
     var sum = 1;
     for (var i = 0; i < arguments.length; i++) {
