8630e4ea74a2e2063648c3e182f7a98d72dca653
Latifa_Simbini <simbinelatifa@gmail.com>
2019-06-10 11:01:03 +0200

    Update password.js

--- password/src/password.js
+++ password/src/password.js
@@ -1,37 +1,42 @@
+
 function password_is_valid(password){
-     
-    var check = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{9,}$/;          
-    var result = check.test(password); 
-  
-   try {
-     if(result == false) throw('password must have atleast one uppercase letter, lowercase letter and number');
-     
-   } catch (err) {
-     return 'Error ' + err;
-  
-   }
-  
-     finally {
-        return result;
-     }
+
+  var check = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{9,}$/;
+  var tester = check.test(password);
+if(tester == true )
+  {
+      return true
+  }
+else
+  {
+      throw new Error
   }
 
-function password_is_ok(password) {
- 
- let exp1 = /^[A-Za-z0-9]{9,}$/;
- let exp2 = /^(?=.*[A-Z])[A-Z]+/;
- let exp3 = /^(?=.*[a-z])[a-z]+/;
- let exp4 = /^(?=.*[0-9])[0-9]+/;
 
- if (exp1.test(password) === true && exp2.test(password) === true) {
-   return 'password is valid';
- } else
- if (exp1.test(password) === true && exp3.test(password) === true) {
-   return 'password is valid';
- } else
- if (exp1.test(password) === true && exp4.test(password) === true) {
-   return 'password is valid'
- } else{
-   return 'password is invalid'
- }
+}
+
+
+function password_is_ok(password) {
+
+let length1 = /^[A-Za-z0-9]{9,}$/;
+let upper = /^(?=.*[A-Z])[A-Z]+/;
+let low = /^(?=.*[a-z])[a-z]+/;
+let digit = /^(?=.*[0-9])[0-9]+/;
+
+if(length1 == false && password === ""){
+ return "password is never OK"
+}
+
+if (length1.test(password) === true && upper.test(password) === true) {
+ return 'password is valid';
+} else
+if (length1.test(password) === true && low.test(password) === true) {
+ return 'password is valid';
+} else
+if (length1.test(password) === true && digit.test(password) === true) {
+ return 'password is valid'
+}
+else{
+ return 'password is invalid'
+}
 }
